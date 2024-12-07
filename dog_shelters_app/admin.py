73900133338f521df2c6151d864c9e514cf11719
erdey2 <erdeysyoum@gmail.com>
2024-12-07 08:22:57 +0000

    Shelter and Dog added to Adminsite

--- dog_shelters_app/admin.py
+++ dog_shelters_app/admin.py
@@ -3,6 +3,6 @@ from . import models
 # Register your models here.
 
 
-admin.AdminSite.register(models.Shelter)
-admin.AdminSite.register(models.Dog)
+admin.site.register(models.Shelter)
+admin.site.register(models.Dog)
 
