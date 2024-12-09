b2ca4c47c31ead007f13210b66e93601fd72e758
erdey2 <erdeysyoum@gmail.com>
2024-12-09 14:35:28 +0000

    shelters list & detail added

--- dog_shelters_app/views.py
+++ dog_shelters_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from dog_shelters_app.models import Shelter
 
 # Create your views here.
+def shelter_list(request):
+    shelters = Shelter.objects.all()
+    context = {'shelters': shelters}
+    return render(request, 'shelter_list.html', context)
+
+def shelter_detail(request, pk):
+    shelter = get_object_or_404(Shelter, pk=pk)
+    context = {'shelter': shelter}
+    return render(request, 'shelter_detail.html', context)
+
