df95c70ae52f843a4eba384d938ad951f5e1733e
erdey2 <erdeysyoum@gmail.com>
2024-12-09 14:34:23 +0000

    database models updated

--- dog_shelters_app/models.py
+++ dog_shelters_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Shelter(models.Model):
+    objects = None
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
 
