c73a80dc032d229c2fc21f30945d6c7e97d8ace4
erdey2 <erdeysyoum@gmail.com>
2024-12-10 12:56:10 +0000

    views.py updated

--- dog_shelters_app/views.py
+++ dog_shelters_app/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from dog_shelters_app.models import Shelter, Dog
+from . import models
+from django.views import generic
 
 # Create your views here.
-def shelter_list(request):
+def shelters_list(request):
     shelters = Shelter.objects.all()
     context = {'shelters': shelters}
-    return render(request, 'shelter_list.html', context)
+    return render(request, 'shelters_list.html', context)
 
 def shelter_detail(request, pk):
-    shelter = get_object_or_404(Shelter, pk=pk)
+    # shelter = get_object_or_404(Shelter, pk=pk)
+    shelter = Shelter.objects.get(pk=pk)
     context = {'shelter': shelter}
     return render(request, 'shelter_detail.html', context)
 
-def dog_list(request):
-    dogs = Dog.objects.all()
-    context = {'dogs': dogs}
-    return render(request, 'dog_list.html', context)
+class DogDetailView(generic.DetailView):
+    model = models.Dog
+    template_name = 'dog_detail.html'
+    context_object_name = 'dog'
 
+class DogsListView(generic.ListView):
+    template_name = 'dogs_list.html'
+    context_object_name = 'dogs'
+
+    def get_queryset(self):
+        return Dog.objects.all()
 
