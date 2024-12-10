bd1d1923dc5e9a27e90868ab8d305d9cb6c5c7ff
erdey2 <erdeysyoum@gmail.com>
2024-12-10 12:55:35 +0000

    urls.py updated

--- dog_shelters_app/urls.py
+++ dog_shelters_app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shelter_list, name='shelter_list'),
+    path('', views.shelters_list, name='shelters_list'),
     path('shelter/<int:pk>', views.shelter_detail, name='shelter_detail'),
-    path('dogs', views.dog_list, name='dog_list'),
+
+    path('dogs', views.DogsListView.as_view(), name='dogs_list'),
+    path('dog/<int:pk>', views.DogDetailView.as_view(), name='dog_detail'),
 ]
 
