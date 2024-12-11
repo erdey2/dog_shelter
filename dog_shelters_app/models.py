202e5567801f4a1fe942f7b64d2e2a167ba83cd8
erdey2 <erdeysyoum@gmail.com>
2024-12-11 12:57:49 +0000

    get_absolute_url method added

--- dog_shelters_app/models.py
+++ dog_shelters_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Shelter(models.Model):
@@ -18,5 +19,8 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('dog_detail', kwargs={'pk': self.pk})
 
 
